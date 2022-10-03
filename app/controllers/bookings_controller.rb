77d0990b4c880d67ae753bc277a93db72f115707
Kyle Hochdoerfer <kjhoch2@gmail.com>
2022-10-03 19:15:43 -0400

    Can't build passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @booked_flight = Flight.find(params[:flight_id])
     passenger_count = params[:passenger_count].to_i
-    @flight_id = params[:flight_id]
+    flight_id = params[:flight_id]
     passenger_count.times { @booking.passengers.build } #this line gives me an error, unknown attribute 'booking_id' for Passenger, but I'm not using a booking id for anything...
   end
 

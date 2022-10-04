22a4ad77cd3ee7919fd35e58344eca3bc98b5656
Kyle Hochdoerfer <kjhoch2@gmail.com>
2022-10-04 10:50:38 -0400

    Trying to build passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,13 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @booked_flight = Flight.find(params[:flight_id])
-    passenger_count = params[:passenger_count].to_i
-    flight_id = params[:flight_id]
-    passenger_count.times { @booking.passengers.build } #this line gives me an error, unknown attribute 'booking_id' for Passenger, but I'm not using a booking id for anything...
+    @passenger_count = params[:passenger_count].to_i
+    @flight_id = params[:flight_id]
+    @passenger_count.times { @booking.passengers.build } 
   end
 
   def create
-    @booking = Booking.create(booking_params) #not quite sure what to do after this. Do I just need to redirect after the create?
+    @booking = Booking.create(booking_params)
     redirect_to @booking
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_003724) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_140501) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_003724) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end

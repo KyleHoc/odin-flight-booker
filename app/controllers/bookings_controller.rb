34f665e1448047dc2c86c4552449e768ec49c6a8
Kyle Hochdoerfer <kjhoch2@gmail.com>
2022-10-04 21:51:21 -0400

    Just need to display Show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,12 +8,16 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.create(booking_params)
-    redirect_to @booking
+    @booking = Booking.new(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:passenger_count, :flight_id, passenger_attributes: [:name, :email])
+    params.require(:booking).permit(:passenger_count, :flight_id, :id, passengers_attributes: [:name, :email])
   end
 end
